4fb47fd60ff5208ffccc1de502db29ca0e24e56b
jim213 <991581463@qq.com>
2019-04-09 15:03:48 +0800

    first commit

---
+++ src/main/java/com/example/mywebsocket/MywebsocketApplication.java
@@ -0,0 +1,14 @@
+package com.example.mywebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MywebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MywebsocketApplication.class, args);
+    }
+
+}
+
